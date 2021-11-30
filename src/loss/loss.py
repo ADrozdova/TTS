f1c0c050815022e6de32d0b04cfe087b410b12b3
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-12-01 00:51:23 +0300

    fix

--- src/loss/loss.py
+++ src/loss/loss.py
@@ -7,9 +7,9 @@ class FastSpeechLoss(nn.Module):
         self.mse_loss = nn.MSELoss()
 
     def forward(self, batch):
-        duration_loss = self.length_mse(batch.durations, batch.duration_pred)
+        duration_loss = self.mse_loss(batch.durations, batch.duration_pred)
 
         len = min(batch.melspec.shape[-1], batch.melspec_prediction.shape[-2])
-        mel_loss = self.mel_spec_mse(batch.melspec[:, :, :len], batch.melspec_pred[:, :, :len])
+        mel_loss = self.mse_loss(batch.melspec[:, :, :len], batch.melspec_pred[:, :, :len])
         loss = mel_loss + duration_loss
         return loss, mel_loss, duration_loss
