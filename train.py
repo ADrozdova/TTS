4d516216c1a480a613ce015f957fe5a8664c6b65
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-12-01 15:25:59 +0300

    loades

--- train.py
+++ train.py
@@ -14,6 +14,13 @@ from src.utils.parse_config import ConfigParser
 from src.vocoder import Vocoder
 from src.featurizer import MelSpectrogram, MelSpectrogramConfig
 from src.aligner import GraphemeAligner
+from torch.utils.data import DataLoader, ChainDataset
+
+from src.datasets.ljspeech import LJSpeechDataset
+from src.collate_fn.collate import LJSpeechCollator
+from src.utils.parse_config import ConfigParser
+
+
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -32,7 +39,12 @@ def main(config):
     aligner = GraphemeAligner()
 
     # setup data_loader instances
-    dataloaders = get_dataloaders(config)
+    # dataloaders = get_dataloaders(config)
+    dataloaders = {}
+    dataset = LJSpeechDataset(".", "train")
+    val_dataset = LJSpeechDataset(".", "val")
+    dataloaders['train'] = DataLoader(dataset, num_workers=4, batch_size=2, collate_fn=LJSpeechCollator())
+    dataloaders['val'] = DataLoader(val_dataset, num_workers=4, batch_size=2, collate_fn=LJSpeechCollator())
 
     # build model architecture, then print to console
     model = config.init_obj(config["arch"], module_arch)
