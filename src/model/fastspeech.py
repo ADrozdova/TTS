285cdd4fa8e825978b383b6f32208c925b82f5e1
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-12-03 22:08:00 +0300

    fix config

--- src/model/fastspeech.py
+++ src/model/fastspeech.py
@@ -5,12 +5,12 @@ from src.model.fastspeech_blocks import FFT, LengthRegulator, PositionalEncoding
 
 
 class FastSpeech(BaseModel):
-    def __init__(self, vocab_size, enc_layers, in_size, n_heads, hidden, kernels, padding, dp_in_size, dp_hidden,
+    def __init__(self, voc_size, enc_layers, in_size, n_heads, hidden, kernels, padding, dp_in_size, dp_hidden,
                  dp_kernel, dec_layers, fc_in, fc_mel, attn_dropout=0.1, dropout=0.1):
         super(FastSpeech, self).__init__()
         self.encoder = FFT(enc_layers, in_size, n_heads, hidden, kernels, padding, attn_dropout=attn_dropout,
                            dropout=dropout)
-        self.emb = nn.Embedding(vocab_size, in_size, padding_idx=0)
+        self.emb = nn.Embedding(voc_size, in_size, padding_idx=0)
         self.pos_enc = PositionalEncoding(in_size, dropout)
 
         self.len_reg = LengthRegulator(dp_in_size, dp_hidden, dp_kernel, dropout=dropout)
