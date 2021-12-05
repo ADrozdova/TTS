be30e0dca99cdf5e961a131ec12ae517f243ec03
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-12-05 14:01:13 +0300

    alignment

---
+++ src/aligner/aligner.py
@@ -0,0 +1,21 @@
+import os
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence
+
+
+class Aligner(nn.Module):
+    def __init__(self, root):
+        super(Aligner, self).__init__()
+        self.root = root
+
+    @torch.no_grad()
+    def forward(self, index):
+        durations = []
+        for idx in index:
+            durations.append(torch.from_numpy(
+                np.load(os.path.join(self.root, str(index) + ".npy"))
+            ))
+        return pad_sequence(durations, batch_first=True)
