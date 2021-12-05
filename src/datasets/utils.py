bf24be6d403967c03aa75a2ddc4e3117fb8c7830
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-12-05 11:52:33 +0300

    shuffle

--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -30,7 +30,7 @@ def get_dataloaders(configs: ConfigParser):
             "You must provide batch_size or batch_sampler for each split"
         if "batch_size" in params:
             bs = params["batch_size"]
-            shuffle = False
+            shuffle = True
             batch_sampler = None
         else:
             raise Exception()
